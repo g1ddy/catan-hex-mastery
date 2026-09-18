07391dbc7090cbb66015bce44985e5c2b15e4fd1
g1ddy <g1ddy@users.noreply.github.com>
2026-09-18 07:52:23 -0400

    chore: remove unrelated changes from rollout fix

--- src/pages/SetupPage.tsx
+++ src/pages/SetupPage.tsx
@@ -46,7 +46,7 @@ export function SetupPage() {
   const startGame = (numPlayers: number, mode: string, numBots: number = 0) => {
     navigate({
         pathname: '/game',
-        search: location.search || window.location.search
+        search: location.search
     }, {
         state: {
             numPlayers,
